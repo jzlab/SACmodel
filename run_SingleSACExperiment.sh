940d0eee515710dfaa1d090afd0f09492336eb52
audrey <tsoi.audrey@gmail.com>
2019-03-25 20:09:03 -0600

    working SingleSACExperiment from src dir

---
+++ run_SingleSACExperiment.sh
@@ -0,0 +1,19 @@
+cd src/
+
+EXP_OUTPUT_DIR="/home/audreyt/data/SACmodel/SingleSACExperimentResults"
+echo "creating output files..."
+touch SingleSACExperimentVvsAngle.txt
+touch SingleSACExperimentVTrace.txt
+
+rm -rf x86_64/
+
+# Compile resources
+echo "Compiling model files..."
+nrnivmodl
+
+echo "Running Experiment..."
+nrngui MainA.hoc
+
+echo "Moving output files to $EXP_OUTPUT_DIR"
+mv SingleSACExperimentVvsAngle.txt $EXP_OUTPUT_DIR/
+mv SingleSACExperimentVTrace.txt $EXP_OUTPUT_DIR/
