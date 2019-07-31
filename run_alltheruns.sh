92dde672d159ce5bd71e07069dd4db6ef3e914da
audrey <tsoi.audrey@gmail.com>
2019-07-31 16:11:59 -0600

    opens and runs tmux then hoc from a sh file, should be able to be applied to many files at once

--- run_alltheruns.sh
+++ run_alltheruns.sh
@@ -1,21 +1,6 @@
 cd src/
-
 tmux new-session -d -s run2
 
-EXP_OUTPUT_DIR="/home/audreyt/data/SACmodel/SingleSACExperimentResults"
-echo "creating output files..."
-touch SingleSACExperimentVvsAngle.txt
-touch SingleSACExperimentVTrace.txt
-
-rm -rf x86_64/
-
-# Compile resources
-echo "Compiling model files..."
-nrnivmodl
+tmux send -t  "run2" 'nrngui -nogui run2.hoc' ENTER
  
-echo "Running Experiment..."
-nrngui -nogui run2.hoc
 
-echo "Moving output files to $EXP_OUTPUT_DIR"
-mv SingleSACExperimentVvsAngle.txt $EXP_OUTPUT_DIR/
-mv SingleSACExperimentVTrace.txt $EXP_OUTPUT_DIR/
