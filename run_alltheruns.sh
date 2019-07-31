53191a41a9abba089a61520fc3efc081e3f5f8bb
Audrey Tsoi <audreytsoi@Audreys-MBA.local>
2019-07-31 15:17:57 -0600

    new session files

---
+++ run_alltheruns.sh
@@ -0,0 +1,21 @@
+cd src/
+
+tmux new-session -d -s run2
+
+EXP_OUTPUT_DIR="/home/audreyt/data/SACmodel/SingleSACExperimentResults"
+echo "creating output files..."
+touch SingleSACExperimentVvsAngle.txt
+touch SingleSACExperimentVTrace.txt
+
+rm -rf x86_64/
+
+# Compile resources
+echo "Compiling model files..."
+nrnivmodl
+ 
+echo "Running Experiment..."
+nrngui -nogui run2.hoc
+
+echo "Moving output files to $EXP_OUTPUT_DIR"
+mv SingleSACExperimentVvsAngle.txt $EXP_OUTPUT_DIR/
+mv SingleSACExperimentVTrace.txt $EXP_OUTPUT_DIR/
