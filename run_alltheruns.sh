0e98ce41c22b27fb3cbb9e7f6f38c3fbc8f761de
Audrey Tsoi <audreytsoi@Audreys-MBA.local>
2019-08-01 12:26:35 -0600

    looping tmuxes

--- run_alltheruns.sh
+++ run_alltheruns.sh
@@ -2,7 +2,7 @@
 cd src/
 
 EXP_OUTPUT_DIR="/home/audreyt/data/SACmodel/SingleSACExperimentResults"
-echo "refreshing output files..."  #will this be okay if it doesn't exist?
+echo "refreshing output files..."  
 touch SingleSACExperimentSpeedsAndContrasts.txt
 
 rm -rf x86_64/
@@ -14,22 +14,21 @@ nrnivmodl
 
 echo "Running Experiment..."
 VAR= "run"
-COUNTER= 1
-echo $COUNTER
-((COUNTER +=1))
-echo $COUNTER
-VARRUN= "$VAR$COUNTER"
-echo $VARRUN
-#tmux new-session -d -s $VARRUN
 
-#tmux send -t  "$VARRUN" 'nrngui -nogui $VARRUN.hoc' ENTER
+for i in {1..10} do 
+VARRUN= "$VAR$i"
+tmux new-session -d -s $VARRUN
+
+tmux send -t  "$VARRUN" 'nrngui -nogui $VARRUN.hoc' ENTER
+done
+
  
 
-tmux new-session -d -s run1
-tmux send -t "run1" 'nrngui -nogui run1.hoc ' ENTER
+#tmux new-session -d -s run1
+#tmux send -t "run1" 'nrngui -nogui run1.hoc ' ENTER
 
-tmux new-session -d -s run2
-tmux send -t "run2" 'nrngui -nogui run2.hoc ' ENTER
+#tmux new-session -d -s run2
+#tmux send -t "run2" 'nrngui -nogui run2.hoc ' ENTER
 
 echo "Moving output files to $EXP_OUTPUT_DIR"
 mv SingleSACExperimentSpeedsAndContrasts.txt $EXP_OUTPUT_DIR/
