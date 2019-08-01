ce40e0b31d71eac460fab89d7e51b4f4beb4da77
Audrey Tsoi <audreytsoi@Audreys-MBA.local>
2019-08-01 11:51:25 -0600

    tmuxing

--- run_alltheruns.sh
+++ run_alltheruns.sh
@@ -7,6 +7,7 @@ rm -rf x86_64/
 echo "Compiling model files..."
 nrnivmodl
  
+ :'
 echo "Running Experiment..."
 VAR= "run"
 VARCOUNTER= "1"
@@ -14,7 +15,15 @@ VARRUN= "$VAR$VARCOUNTER"
 tmux new-session -d -s $VARRUN
 
 tmux send -t  "$VARRUN" 'nrngui -nogui $VARRUN.hoc' ENTER
- 
+ '
+echo "Running Experiment..."
+tmux new-session -d -s run1
+tmux send -t "run1" 'nrngui -nogui run1.hoc ' ENTER
+
+tmux new-session -d -s run2
+tmux send -t "run2" 'nrngui -nogui run2.hoc ' ENTER
+
+
 
 
 
