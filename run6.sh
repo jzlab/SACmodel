7ed22910fd75205eafb8e007b72b5de9c63b6163
Audrey Tsoi <audreytsoi@Audreys-MBA.local>
2019-08-08 16:30:33 -0600

    run6.sh file

---
+++ run6.sh
@@ -0,0 +1,19 @@
+cd src/
+
+EXP_OUTPUT_DIR="/home/audreyt/data/SACmodel/SingleSACExperimentResults"
+echo "creating output files..."
+touch EndTraces.txt
+touch V_CAI_TuningCurves.txt
+
+rm -rf x86_64/
+
+# Compile resources
+echo "Compiling model files..."
+nrnivmodl
+ 
+echo "Running Experiment..."
+nrngui -nogui run6.hoc
+
+echo "Moving output files to $EXP_OUTPUT_DIR"
+mv EndTraces.txt $EXP_OUTPUT_DIR/
+mv V_CAI_TuningCurves.txt $EXP_OUTPUT_DIR/
